2f3dfe2b98766499093215859d0202301e340261
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-04-01 23:27:46 +0530

    refactor: comment out Timer component and add counterSlice implementation

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-function Timer() {
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log("Running...");
-    }, 1000);
+// function Timer() {
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       console.log("Running...");
+//     }, 1000);
 
-    return () => {
-      clearInterval(interval);
-      console.log("Cleanup done");
-    };
-  }, []);
+//     return () => {
+//       clearInterval(interval);
+//       console.log("Cleanup done");
+//     };
+//   }, []);
 
-  return <h1>Timer Running</h1>;
-}
+//   return <h1>Timer Running</h1>;
+// }
 
-export default Timer;
+// export default Timer;
+
+
+// learn redus toolkit 
+
+// counterSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
